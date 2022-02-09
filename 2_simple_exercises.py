cdf13f630d2d6586d05f64750b59d688d732c01a
James Dickson <james_dickson1@baylor.edu>
2022-02-09 15:47:03 -0600

    commit

--- 2_simple_exercises.py
+++ 2_simple_exercises.py
@@ -45,4 +45,6 @@ print(sampleDict)
 
 dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
 
+dict["work"] = ["Apology", "Phaedo", "Republic", "Symposium"]
 
+print(dict)
