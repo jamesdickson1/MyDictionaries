48d75d1f8dd05192fde8059aa9a266a4633d4d47
James Dickson <james_dickson1@baylor.edu>
2022-02-14 14:45:03 -0600

    commit

--- 4_datastore.py
+++ 4_datastore.py
@@ -47,3 +47,14 @@ datastore = { "medical":[
 
       ]
 }
+
+outfile = open('retail_space.csv','w')
+outfile.write("room-number,use,sq-ft,price\n")
+
+
+#print(datastore["medical"])
+
+for i in datastore["medical"]:
+  outfile.write(str(i['room-number']) + "," + i["use"] + "," + str(i["sq-ft"]) + "," + str(i["price"]) + "\n")
+
+outfile.close()
